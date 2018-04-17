83d8aa9d267fe533a8d2e7e0ac327ebba958aa65
Ryo Kawamura <ryo@rectlabel.com>
2018-04-18 06:50:18 +0900

    Added convert_csv_to_sframe.py

---
+++ convert_csv_to_sframe.py
@@ -0,0 +1,33 @@
+import turicreate as tc
+from turicreate import SArray
+import pandas as pd
+import os
+import sys
+
+csv_file = sys.argv[1]
+
+csv = pd.read_csv(csv_file)
+for i, row in csv.iterrows():
+    image_folder = str(os.path.split(row['path'])[0])
+    break;
+
+data = tc.image_analysis.load_images(image_folder, with_path=True)
+
+annotations = []
+for j, item in enumerate(data):
+    has_annotation = False
+    for i, row in csv.iterrows():
+        if str(row['path']) == item['path']:
+            annotations.append(eval(row['annotations']))
+            has_annotation = True
+            break
+    if not has_annotation:
+        annotations.append([])
+
+data['annotations'] = SArray(data=annotations, dtype=list)
+
+data.save('annotations.sframe')
+print(data)
+
+
+
