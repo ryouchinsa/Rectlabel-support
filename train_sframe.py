e679ffdad2649f79ff4197843774815dfd2b15ee
Ryo Kawamura <ryo@rectlabel.com>
2019-03-27 18:25:48 +0900

    Added train_turicreate.py

--- train_sframe.py
+++
@@ -1,5 +0,0 @@
-import turicreate as tc
-
-train_data =  tc.SFrame('annotations.sframe')
-model = tc.object_detector.create(train_data, feature = 'image', max_iterations=100)
-model.save('mymodel.model')
