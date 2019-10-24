a790c660cb75ea22365bda86224d1a37023fde7b
Ryo Kawamura <ryo@rectlabel.com>
2019-10-25 07:45:12 +0900

    Update

--- dicom_to_images.py
+++ dicom_to_images.py
@@ -11,7 +11,7 @@ print(ds)
 
 dcm_filename = os.path.splitext(dcm_file)[0]
 for idx, pixel in enumerate(ds.pixel_array):
-	shape = pixel.shape
+    shape = pixel.shape
 	image_2d = pixel.astype(float)
 	image_2d = (np.maximum(image_2d, 0) / image_2d.max()) * 255.0
 	image_2d = np.uint8(image_2d)
@@ -24,4 +24,4 @@ for idx, pixel in enumerate(ds.pixel_array):
 		with open(path_png, 'wb') as file:
 		    w = png.Writer(shape[1], shape[0], greyscale=True)
 		    w.write(file, image_2d)
-		
+
