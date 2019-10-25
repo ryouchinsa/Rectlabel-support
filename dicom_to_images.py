274f406afbebb08924cc9df67fcb62d69046d407
Ryo Kawamura <ryo@rectlabel.com>
2019-10-26 08:27:30 +0900

    Update

--- dicom_to_images.py
+++ dicom_to_images.py
@@ -8,17 +8,17 @@ save_as_jpg = True
 dcm_file = sys.argv[1]
 output_folder = sys.argv[2]
 
-def from2dArray(dcm_filename, idx, pixel):
+def from2dArray(dcm_filename, frame_idx, pixel):
     shape = pixel.shape
     image_array = pixel.astype(float)
     image_array = (np.maximum(image_array, 0) / image_array.max()) * 255.0
     image_array = np.uint8(image_array)
     if save_as_jpg:
-        path_jpg = output_folder + '/' + dcm_filename + '_' + str(idx) + '.jpg'
+        path_jpg = output_folder + '/' + dcm_filename + '_' + str(frame_idx) + '.jpg'
         im = PIL.Image.fromarray(image_array)
         im.save(path_jpg)
     else:
-        path_png = output_folder + '/' + dcm_filename + '_' + str(idx) + '.png'
+        path_png = output_folder + '/' + dcm_filename + '_' + str(frame_idx) + '.png'
         with open(path_png, 'wb') as file:
             w = png.Writer(shape[1], shape[0], greyscale=True)
             w.write(file, image_array)
@@ -26,13 +26,20 @@ def from2dArray(dcm_filename, idx, pixel):
 ds = pydicom.dcmread(dcm_file)
 dcm_filename = os.path.splitext(os.path.basename(dcm_file))[0]
 print(ds)
-print(ds.pixel_array.shape)
+print(ds.Rows)
+print(ds.Columns)
+print(ds.SamplesPerPixel)
+if 'NumberOfFrames' in ds:
+    print(ds.NumberOfFrames)
 
-if ds.pixel_array.ndim == 2:
-    idx = 0
-    from2dArray(dcm_filename, idx, ds.pixel_array)
+frame_idx = 0
+if not 'NumberOfFrames' in ds:
+    from2dArray(dcm_filename, frame_idx, ds.pixel_array)
+elif ds.NumberOfFrames == 1:
+    from2dArray(dcm_filename, frame_idx, ds.pixel_array)
 else:
-    for idx, pixel in enumerate(ds.pixel_array):
-        from2dArray(dcm_filename, idx, pixel)
+    for frame_idx, pixel in enumerate(ds.pixel_array):
+        from2dArray(dcm_filename, frame_idx, pixel)
+    
 
 
