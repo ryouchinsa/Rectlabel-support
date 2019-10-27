45e37e9316c218e3eed0c990d35439254a863d13
Ryo Kawamura <ryo@rectlabel.com>
2019-10-28 08:59:08 +0900

    Update

--- checkIndexColorPalette.py
+++ checkIndexColorPalette.py
@@ -1,15 +1,17 @@
 import PIL.Image
 import numpy as np
+import sys
 
-if __name__ == '__main__':
-    image_path = '/Users/ryo/Desktop/test_annotations/mask/tuce-311434-unsplash_all_objects.png';
-    try:
-        image = PIL.Image.open(image_path, 'r')
-        print(image.mode)
-        palette = image.getpalette()
-        palette = np.array(palette).reshape(-1, 3)
-        print(palette)
-    except IOError:
-        print("IOError", image_path)
+np.set_printoptions(threshold=sys.maxsize)
 
-        
+image_path = '/Users/ryo/Desktop/test_annotations/test_data_mask/index_color_image/23823333-189975791565259-1963930219081367552-n_all_objects.png'
+try:
+    image = PIL.Image.open(image_path, 'r')
+    print(image.mode)
+    img = np.array(image)
+    print(img[np.where(img > 0)])
+    palette = image.getpalette()
+    palette = np.array(palette).reshape(-1, 3)
+    print(palette)
+except IOError:
+    print("IOError", image_path)
--- checkPycocoDemo.py
+++ checkPycocoDemo.py
@@ -16,26 +16,19 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
-imgFolder = '/Users/ryo/Desktop/test_annotations/test_data_coco/'
-annFile='/Users/ryo/Desktop/test_annotations/test_data_coco/annotations.json'
+imgFolder = '/Users/ryo/Desktop/test_annotations/test_data_donut/'
+annFile='/Users/ryo/Desktop/test_annotations/test_data_donut/annotations.json'
+imageIdToShow = 1
 
 coco=COCO(annFile)
-imgIds = coco.getImgIds(imgIds = [1, 2])
-img = coco.loadImgs(imgIds[0])[0]
+img = coco.loadImgs(imageIdToShow)[0]
 I = io.imread(imgFolder + img['file_name'])
 plt.axis('off')
 plt.imshow(I);
 
-catIds = list(range(1, 100))
+catIds = list(range(1, 256))
 annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
 anns = coco.loadAnns(annIds)
-
-for id, ann in enumerate(anns):
-    bb = ann['bbox']
-    x1, x2, y1, y2 = [bb[0], bb[0]+bb[2], bb[1], bb[1]+bb[3]]
-    ann['segmentation'] = [[x1, y1, x1, y2, x2, y2, x2, y1]]
-pp.pprint(anns)
-
 coco.showAnns(anns)
 plt.show()
 
