5569e4539d4f4d7c073871ddca7cfdec5ed57cd0
Ryo Kawamura <ryo@rectlabel.com>
2019-11-14 08:35:25 +0900

    Added rectlabel_detect_with_tf_model.py

---
+++ rectlabel_detect_with_tf_model.py
@@ -0,0 +1,92 @@
+
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import dataset_util
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+result_dir = '/Users/ryo/Desktop/test_annotations/raccoon_dataset-master/result'
+def makedir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+makedir(result_dir)
+
+test_txt_path = '/Users/ryo/Desktop/test_annotations/raccoon_dataset-master/test.txt'
+test_images = dataset_util.read_examples_list(test_txt_path)
+
+PATH_TO_LABELS = '/Users/ryo/Desktop/test_annotations/raccoon_dataset-master/label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+PATH_TO_FROZEN_GRAPH = '/Users/ryo/Desktop/test_annotations/raccoon_dataset-master/export/frozen_inference_graph.pb'
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+def run_inference_for_single_image(image, graph):
+    with graph.as_default():
+        with tf.Session() as sess:
+            ops = tf.get_default_graph().get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            tensor_dict = {}
+            for key in ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes', 'detection_masks']:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+            if 'detection_masks' in tensor_dict:
+                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+                detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+                detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(detection_masks, detection_boxes, image.shape[1], image.shape[2])
+                detection_masks_reframed = tf.cast(tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
+            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+            output_dict = sess.run(tensor_dict, feed_dict={image_tensor: image})
+            output_dict['num_detections'] = int(output_dict['num_detections'][0])
+            output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.int64)
+            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+            output_dict['detection_scores'] = output_dict['detection_scores'][0]
+            if 'detection_masks' in output_dict:
+                output_dict['detection_masks'] = output_dict['detection_masks'][0]
+            return output_dict
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+for image_path in test_images:
+    print('# processing ... ' + image_path)
+    image = Image.open(image_path)
+    image_np = load_image_into_numpy_array(image)
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    plt.figure(figsize=IMAGE_SIZE)
+    plt.imshow(image_np)
+    image_path_split = image_path.split('/')
+    image_filename = os.path.splitext(image_path_split[-1])[0]
+    save_path = os.path.join(result_dir, image_filename + '.jpg')
+    plt.savefig(save_path)
+print('# Finished.')
