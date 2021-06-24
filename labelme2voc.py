eb07b62b0a6587d9c94561efb46744ba924fe753
Ryo Kawamura <ryo@rectlabel.com>
2021-06-24 10:55:54 +0900

    Added labelme2voc.py

---
+++ labelme2voc.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import argparse
+import glob
+import os
+import os.path as osp
+import sys
+
+import labelme
+
+try:
+    import lxml.builder
+    import lxml.etree
+except ImportError:
+    print("Please install lxml:\n\n    pip install lxml\n")
+    sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("input_dir", help="input directory for images and json files")
+    parser.add_argument("output_dir", help="output directory for xml files")
+    args = parser.parse_args()
+    if not osp.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    for filename in glob.glob(osp.join(args.input_dir, "*.json")):
+        print("Generating dataset from:", filename)
+        label_file = labelme.LabelFile(filename=filename)
+        base = osp.splitext(osp.basename(filename))[0]
+        out_xml_file = osp.join(args.output_dir, base + ".xml")
+        img = labelme.utils.img_data_to_arr(label_file.imageData)
+        maker = lxml.builder.ElementMaker()
+        xml = maker.annotation(
+            maker.folder(),
+            maker.filename(base + ".jpg"),
+            maker.size(
+                maker.height(str(img.shape[0])),
+                maker.width(str(img.shape[1])),
+                maker.depth(str(img.ndim)),
+            ),
+        )
+        for shape in label_file.shapes:
+            if shape["shape_type"] != "polygon":
+                print(
+                    "Skipping shape: label={label}, "
+                    "shape_type={shape_type}".format(**shape)
+                )
+                continue
+
+            polygon = maker.polygon()
+            for i, xy in enumerate(shape["points"]):
+                x, y = xy
+                polygon.append(lxml.etree.XML("<x" + str(i + 1) + ">" + str(x) + "</x" + str(i + 1) + ">"))
+                polygon.append(lxml.etree.XML("<y" + str(i + 1) + ">" + str(y) + "</y" + str(i + 1) + ">"))
+
+            xml.append(
+                maker.object(
+                    maker.name(shape["label"]),
+                    polygon,
+                )
+            )
+
+        with open(out_xml_file, "wb") as f:
+            f.write(lxml.etree.tostring(xml, pretty_print=True))
+
+
+if __name__ == "__main__":
+    main()
