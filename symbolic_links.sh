60fa0bf6752bc655738551702a5597f2c4140a7d
Ryo Kawamura <ryo@rectlabel.com>
2022-02-24 13:21:24 +0900

    added a creating symbolic links script

---
+++ symbolic_links.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 srcFolder dstFolder" >&2
+  exit 1
+fi
+srcFolder=$1
+dstFolder=$2
+imgExtensionList="jpg jpeg JPG png PNG"
+for file in "$srcFolder"/*
+do
+  filename=$(basename -- "$file")
+  extension="${filename##*.}"
+  for imgExtension in $imgExtensionList
+  do
+    if [ "$extension" == "$imgExtension" ]; then
+      dstFile="${dstFolder}/${filename}"
+      ln -s "$file" "$dstFile"
+      break
+    fi
+  done
+done
