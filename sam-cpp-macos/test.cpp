17d7e753484157e87c134d6978092a431551c9d6
Ryo Kawamura <ryo@rectlabel.com>
2023-09-02 05:16:33 +0900

    Update test.cpp

--- sam-cpp-macos/test.cpp
+++ sam-cpp-macos/test.cpp
@@ -41,21 +41,6 @@ int main(int argc, char** argv) {
   mask = sam.getMask(points, nagativePoints, roi, previousMaskIdx, isNextGetMask);
   previousMaskIdx++;
   cv::imwrite("mask-object1-click2.png", mask);
-  // 1st object and 3rd click
-  points.push_back({855, 405});
-  mask = sam.getMask(points, nagativePoints, roi, previousMaskIdx, isNextGetMask);
-  previousMaskIdx++;
-  cv::imwrite("mask-object1-click3.png", mask);
-  // 1st object and 4th click
-  points.push_back({755, 415});
-  mask = sam.getMask(points, nagativePoints, roi, previousMaskIdx, isNextGetMask);
-  previousMaskIdx++;
-  cv::imwrite("mask-object1-click4.png", mask);
-  // 1st object and 5th click
-  points.push_back({710, 470});
-  mask = sam.getMask(points, nagativePoints, roi, previousMaskIdx, isNextGetMask);
-  previousMaskIdx++;
-  cv::imwrite("mask-object1-click5.png", mask);
   // 2nd object and 1st click
   isNextGetMask = true;
   points.clear();
@@ -69,11 +54,6 @@ int main(int argc, char** argv) {
   mask = sam.getMask(points, nagativePoints, roi, previousMaskIdx, isNextGetMask);
   previousMaskIdx++;
   cv::imwrite("mask-object2-click2.png", mask);
-  // 2nd object and 3rd click
-  points.push_back({715, 250});
-  mask = sam.getMask(points, nagativePoints, roi, previousMaskIdx, isNextGetMask);
-  previousMaskIdx++;
-  cv::imwrite("mask-object2-click3.png", mask);
   // 1st object and box
   isNextGetMask = true;
   points.clear();
